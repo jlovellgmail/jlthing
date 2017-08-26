91cd82301acaa730c5d904262eab2466076b4d2e
jl90000 <jacklovell@gmail.com>
2017-08-25 18:53:40 -0700

    Add Mom's writing to Oysters page

--- script_page.js
+++ script_page.js
@@ -199,14 +199,14 @@ function makeitems(){
 		contentlist.oysters = ""
 			+ img("images/page_images/oysters_2.jpg")
 			+ img("images/page_images/oysters_oysters - Screen shot 2012-09-10 at 3.53.03 AM.jpg")
-			+ text("Grant-funded data visualization research projects in collaboration with researchers at University of Rhode Island and Rhode Island School of Design.")
+			+ text("Collaboration with researchers at the University of Rhode Island Department of Oceanography.")
 			+ text("The problem: ", "heading")
 			+ text("Oyster farming, a large industry in Rhode Island, has been damaged by an infectious disease that is drastically reducing populations. But one strain of oyster is immune. Scientists are working to discover what makes these oysters different.")
 			+ text("Challenges: ", "heading")
-			+ text("When we project data into a readable form, it can look very different depending on how we view it. We have to choose a good visual and conceptual projection in order to see features in the data that matter to us.")
+			+ text("When we project data into a readable form, it can look very different depending on how we view it.  For example, one might wish to look at the interrelatedness of specific genes and/or the timing of when certain genes express themselves in the developing oyster. We have to choose a good visual and conceptual projection in order to see features in the data that matter to us.")
 			+ img("images/page_images/oysters_perspectives.jpg")
 			+ text("Solution:", "heading")
-			+ text('We built software tools that visualize genes as networks of nodes and edges. The visualization allowed scientists to test their assumptions by "playing" with the networks, in order to reveal structure in the data. The software had to be flexible enough to accommodate yet-unknown forms of data, as the scientists continually restructured the material that the software consumed.')
+			+ text('Software that enabled the researchers to visualize genes as networks of "nodes and edges". The visualization allowed scientists to test their assumptions by "playing" with the networks, in order to reveal structure in the data. The software had to be flexible enough to accommodate yet-unknown forms of data, as the scientists continually restructured their material. As a major benefit, the programs have made it much easier to describe and share the progress of this research in educational settings, with colleagues, across disciplines, and in the scientific literature.')
 			+ img("images/page_images/oysters_slider.gif")
 			//+ '<iframe src="https://player.vimeo.com/video/170709062?autoplay=1&amp;loop=1&amp;autopause=0" width="640" height="88" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 			+ img("images/page_images/oysters_4.jpg")
