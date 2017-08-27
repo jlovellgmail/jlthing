fc44308bfa1b33145908cb970e4ccc9023fcfa06
jl90000 <jacklovell@gmail.com>
2017-08-27 01:54:47 -0700

    change to spacers for vertical separation

--- data.js
+++ data.js
@@ -24,9 +24,12 @@ function makedata(){
 			else return "<div class='"+classes+"'>"+contents+"</div>";
 		}
 		var spacer = div("spacer");
+		var spacermedium = div("spacermedium");
+		var spacersmall = div("spacersmall");
 		contentlist.basespace = ""
 			+ '<img class="darkenImage" src="images/page_images/icons.jpg">'
 			+ caption("App icons")
+			+ spacer
 			+ img("images/page_images/developerstats.jpg")
 			+ caption("Analytics dashboard")
 			;
@@ -63,65 +66,90 @@ function makedata(){
 			+ '<div class="aspectratio horses"><iframe src="https://player.vimeo.com/video/231226964?autoplay=1&loop=1&autopause=0" width="640" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>'
 			+ img("images/page_images/gp_targethorse.jpg")
 			+ img("images/page_images/gp_horse.jpg")
-			// + spacer
+			+ spacermedium
 			+ text("Stochastic process", "heading")
 			+ text("Say we have a photo we want to render called the \"target image\". Genetic computer programs seek to replicate this target image through self-generated experimentation. The program process begins by casting random lines across the digital canvas. Most lines don't come even close to the target, but those that come closest are retained by the program and those which don't are discarded. Now we have a new \"generation\" of information and, based on that information, the program refines its aim to more and more accurately reproduce the target image.")
 			+ text("Just as in nature, creating a rendering of a photograph with this process involves many generations where individuals, and in this case \"lines\", mate to see if they can create an improved offspring. Literally billions of organisms go extinct and only rarely does a beneficial mutation appear. But unlike nature, the computer can do this very quickly.")
+			+ spacermedium
 			+ '<div class="aspectratio hand"><iframe src="https://player.vimeo.com/video/169456761?autoplay=1&loop=1&autopause=0" width="640" height="760" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><div class="movieOverlay"></div></div>'
+			+ spacer
 			+ img("images/page_images/gp_hand.jpg")
+			+ spacer
 			+ img("images/page_images/gp_woman.jpg")
+			+ spacer
 			+ img("images/page_images/gp_camera.jpg")
+			+ spacer
 			+ img("images/page_images/gp_triangles.png")
-			//+ img("images/page_images/gp_1304121925.cumulative_best.21400.canvas.jpg")
+			+ spacer
 			+ img("images/page_images/gp_1211080-230.best.1009.fullscreen_crop_945.jpg")
+			+ spacer
 			+ img("images/page_images/gp_1211080-230.best.1009.fullscreen_target_945.jpg")
-
-			//+ '<div class="aspectratio gpshapes"><iframe src="https://player.vimeo.com/video/104474094?autoplay=1&amp;loop=1&amp;title=0&amp;byline=0&amp;portrait=0" width="640" height="384" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><div class="movieOverlay"></div></div>'
+			+ spacer
 			+ '<div class="aspectratio gpshapes"><iframe src="https://player.vimeo.com/video/104474094?autoplay=1&loop=1&autopause=0&title=0&byline=0&portrait=0" width="640" height="384" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe><div class="movieOverlay"></div></div>'
-
 			+ caption("Deforming 3D objects with genetic programs")
 			+ spacer
 			;
 		contentlist.ads = ""
 			+ img("images/page_images/ad_set.jpg")
+			+ spacer
 			+ img("images/page_images/ad_startsmart.jpg")
+			+ spacer
 			+ img("images/page_images/ad_secondwind.jpg")
+			+ spacer
 			+ img("images/page_images/ad_theonly.jpg")
+			+ spacer
 			+ img("images/page_images/ad_onlysit.jpg")
+			+ spacer
 			+ img("images/page_images/ad_earlycomp1.jpg")
+			+ spacer
 			+ img("images/page_images/ad_headlines_1500.jpg")
 			+ caption("Exploring line break variations")
 			;
 		contentlist.bags = ""
 			+ img("images/page_images/bags_SFclutch_1800.jpg")
+			+ spacer
 			+ img("images/page_images/bags_SF-Overnight---2017-01-27-18.31.41---thumbnail_repositioned_1800.jpg")
+			+ spacer
 			+ img("images/page_images/bags_SF-Satchel---2017-01-24-23.35.12---thumbnail_repositioned_1800.jpg")
+			+ spacer
 			+ img("images/page_images/bags_grid.jpg")
+			+ spacer
 			+ img("images/page_images/bags_IMG_8892_1200.jpg")
+			+ spacer
 			+ img("images/page_images/bags_2016-12-08-13.04.20_rt2_50_q9_1200.jpg")
+			+ spacer
 			+ img("images/page_images/bags_2016-11-28-13.21.43---drawstring-top-edge_1200.jpg")
 			;
 		contentlist.event = ""
 			+ text("Hershey's Easter Event", "heading")
 			+ text("Step and repeat, kiosks and stage backdrop.")
+			+ spacermedium
 			+ img("images/page_images/hersheys_double.jpg")
-			+ img("images/page_images/hersheys_stage.jpg")
 			+ spacer
+			+ img("images/page_images/hersheys_stage.jpg")
 			+ spacer
 			+ text("Hershey's BlogHer Conference Suites", "heading")
 			+ text("Floor-to-ceiling environmental graphics.")
+			+ spacermedium
 			+ img("images/page_images/event_perspectivethreewalls.png")
-			+ img("images/page_images/event_rightwall.jpg")
-			+ img("images/page_images/event_wallgraphic_door.jpg")
-			+ img("images/page_images/event_wall_beach.jpg")
-			+ img("images/page_images/event_blogher2012_wallD_2000.jpg")
-			+ img("images/page_images/event_blogher2012_wallD_detail_1500.jpg")
-			+ img("images/page_images/event_blogher2012_wallE_2000.jpg")
-			+ img("images/page_images/event_forest.jpg")
 			+ spacer
+			+ img("images/page_images/event_rightwall.jpg")
+			+ spacer
+			+ img("images/page_images/event_wallgraphic_door.jpg")
+			+ spacer
+			+ img("images/page_images/event_wall_beach.jpg")
+			+ spacer
+			+ img("images/page_images/event_blogher2012_wallD_2000.jpg")
+			+ spacer
+			+ img("images/page_images/event_blogher2012_wallD_detail_1500.jpg")
+			+ spacer
+			+ img("images/page_images/event_blogher2012_wallE_2000.jpg")
+			+ spacer
+			+ img("images/page_images/event_forest.jpg")
 			+ spacer
 			+ text("Aramark Guest Chef Event", "heading")
 			+ text("Entrance and sponsor banners.")
+			+ spacermedium
 			+ img("images/page_images/event_aramark.jpg")
 			;
 		contentlist.oysters = ""
@@ -129,40 +157,38 @@ function makedata(){
 			+ text("Oyster farming, a large industry in Rhode Island, has been damaged by an infectious disease that is drastically reducing populations. But one strain of oyster is immune. Scientists are working to discover what makes these oysters different.")
 			+ text("Challenges: ", "heading")
 			+ text("When we project data into a readable form, it can look very different depending on how we view it.  For example, one might wish to look at the interrelatedness of specific genes and/or the timing of when certain genes express themselves in the developing oyster. We have to choose a good visual and conceptual projection in order to see features in the data that matter to us.")
+			+ spacermedium
 			+ img("images/page_images/oysters_2.jpg")
+			+ spacer
 			+ img("images/page_images/oysters_oysters - Screen shot 2012-09-10 at 3.53.03 AM.jpg")
+			+ spacer
 			+ img("images/page_images/oysters_perspectives.jpg")
 			+ div("caption credit", 'Credit: Emok via <a href="https://commons.wikimedia.org/wiki/File%3AFirst_angle_projection.svg">Wikimedia Commons</a>')
 			+ spacer
 			+ text("Solution:", "heading")
 			+ text('Software that enabled the researchers to visualize genes as networks of "nodes and edges". The visualization allowed scientists to test their assumptions by "playing" with the networks, in order to reveal structure in the data. The software had to be flexible enough to accommodate yet-unknown forms of data, as the scientists continually restructured their material. As a major benefit, the programs have made it much easier to describe and share the progress of this research in educational settings, with colleagues, across disciplines, and in the scientific literature.')
+			+ spacermedium
 			//+ img("images/page_images/oysters_slider.gif")
 			//+ '<iframe src="https://player.vimeo.com/video/170709062?autoplay=1&amp;loop=1&amp;autopause=0" width="640" height="88" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
 			+ img("images/page_images/oysters_4.jpg")
+			+ spacer
 			+ img("images/page_images/oysters1.jpg")
+			+ spacer
 			+ img("images/page_images/oysters2.jpg")
 			;
-			/*
-		contentlist.fayum = ""
-			+ img("images/page_images/fayum.jpg")
-			;
-			*/
 		contentlist.raytrace = ""
 			+ img("images/page_images/raytrace.jpg")
 			+ text("Capabilities include rendering of spheres and polygons, antialiasing, recursive reflections, refraction within transparent objects, texture mapping, antialiasing, and depth of field blurring.")
 			;
-			/*
-		contentlist.developerstats = ""
-			+ img("images/page_images/developerstats.jpg")
-			;
-			*/
 		contentlist.rd = ""
 			+ img("images/page_images/keyart_rd_1600.jpg")
-			+ "<p class='caption'>Key art for the documentary <span style='font-style:italic;'>Racing Dreams</span></p>"
+			+ "<p class='caption'>Key art for the documentary film <span style='font-style:italic;'>Racing Dreams</span></p>"
+			+ spacer
 			+ img("images/page_images/keyart_skinnydip.jpg")
-			+ "<p class='caption'>Key art for <span style='font-style:italic;'>Skinny Dip</span></p>"
+			+ "<p class='caption'>Key art for the film <span style='font-style:italic;'>Skinny Dip</span></p>"
+			+ spacer
 			+ img("images/page_images/keyart_vegucated.jpg")
-			+ "<p class='caption'>Key art for the documentary <span style='font-style:italic;'>Vegucated</span></p>"
+			+ "<p class='caption'>Key art for the documentary film <span style='font-style:italic;'>Vegucated</span></p>"
 			;
 	}
 
@@ -257,17 +283,6 @@ function makedata(){
 			,"Collaboration with researchers at the University of Rhode Island Department of Oceanography"
 			,contentlist.oysters
 		);
-		/*
-		add(
-			"fayum_th.jpg"
-			,"Scientific Illustration"
-			,"fayum"
-			,"Scientific Illustration"
-			,"The American Museum of Natural History, NYC"
-			,contentlist.fayum
-		);
-		*/
-		//------------------------------------
 		add(
 			"raytrace_th.jpg"
 			,"Raytracer"
@@ -276,16 +291,6 @@ function makedata(){
 			,"Written from scratch"
 			,contentlist.raytrace
 		);
-		/*
-		add(
-			"developerstats_th.jpg"
-			,"Analytics Dashboard"
-			,"developerstats"
-			,"Analytics Dashboard UI"
-			,"Illumina BaseSpace app store"
-			,contentlist.developerstats
-		);
-		*/
 	}
 
 }
