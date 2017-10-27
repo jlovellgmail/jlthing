bfc7917b1b50546d82d2b10f679590e81456bfed
jl90000 <jacklovell@gmail.com>
2017-10-26 18:10:39 -0700

    content

--- data.js
+++ data.js
@@ -137,6 +137,15 @@ function makedata(){
 			+ spacer
 			;
 		contentlist.ads = ""
+
+			+ text("My role:", " ", "font-weight:bold;")
+			+ text("Art Direction. I took already-finalized headline and body copy, and designed and executed all visual aspects including photo selection, visual treatment and typography. I worked closely with the copywriter and creative director to ensure the ads stayed on-strategy and spoke in the intended voice.", " ", "margin-top:2px;")
+
+			+ spacersmall
+			+ "<span style='font-weight:bold'>Copywriter:</span> <a target='_blank' href='https://www.jessbrophy.com/#/health-warrior/'>Jessica Brophy</a>"
+
+			+ spacer
+			
 			+ img("images/page_images/ad_set.jpg")
 			+ spacer
 			+ img("images/page_images/ad_startsmart.jpg")
@@ -321,6 +330,14 @@ function makedata(){
 			,contentlist.bags
 		);
 		add(
+			"ad_th.jpg"
+			,"Ad Campaign"
+			,"ad"
+			,"Ad Campaign"
+			,"Spec ads for Health Warrior"
+			,contentlist.ads
+		);
+		add(
 			"hersheys_th.jpg"
 			,"Event Graphics"
 			,"event"
@@ -365,14 +382,6 @@ function makedata(){
 			,"Key Art"
 			,"Film branding"
 			,contentlist.rd
-		);
-		add(
-			"ad_th.jpg"
-			,"Ad Campaign"
-			,"ad"
-			,"Ad Campaign"
-			,"Spec ads for Health Warrior"
-			,contentlist.ads
 		);
 		add(
 			
